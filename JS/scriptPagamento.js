3306de92ea6be3732a1c9662f754daf365d4e7b7
joao <joaoaugustomendes12@gmail.com>
2022-06-26 18:35:27 -0300

    .

--- JS/scriptPagamento.js
+++ JS/scriptPagamento.js
@@ -1,64 +1,14 @@
-function limpa_formulário_cep() {
-    //Limpa valores do formulário de cep.
-    document.getElementById('rua').value=("");
-    document.getElementById('bairro').value=("");
-    document.getElementById('cidade').value=("");
-    document.getElementById('uf').value=("");
-}
-
-function meu_callback(conteudo) {
-if (!("erro" in conteudo)) {
-    //Atualiza os campos com os valores.
-    document.getElementById('rua').value=(conteudo.rua);
-    document.getElementById('bairro').value=(conteudo.bairro);
-    document.getElementById('cidade').value=(conteudo.localidade);
-    document.getElementById('uf').value=(conteudo.uf);
-} //end if.
-else {
-    //CEP não Encontrado.
-    limpa_formulário_cep();
-    alert("CEP não encontrado.");
-}
-}
-
-function pesquisacep(valor) {
-
-//Nova variável "cep" somente com dígitos.
-var cep = valor.replace(/\D/g, '');
-
-//Verifica se campo cep possui valor informado.
-if (cep != "") {
-
-    //Expressão regular para validar o CEP.
-    var validacep = /^[0-9]{8}$/;
-
-    //Valida o formato do CEP.
-    if(validacep.test(cep)) {
-
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('rua').value="...";
-        document.getElementById('bairro').value="...";
-        document.getElementById('cidade').value="...";
-        document.getElementById('uf').value="...";
-
-        //Cria um elemento javascript.
-        var script = document.createElement('script');
-
-        //Sincroniza com o callback.
-        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
-
-        //Insere script no documento e carrega o conteúdo.
-        document.body.appendChild(script);
-
-    } //end if.
-    else {
-        //cep é inválido.
-        limpa_formulário_cep();
-        alert("Formato de CEP inválido.");
+function pagamento() {
+    let data = {
+        nome_cartao: document.getElementById('nome_cartao').value,
+        num_cartao: document.getElementById('num_cartao').value,
+        validade: document.getElementById('validade').value,
+        cvc: document.getElementById('cvc').value,
+        cpf: document.getElementById('cpf').value
     }
-} //end if.
-else {
-    //cep sem valor, limpa formulário.
-    limpa_formulário_cep();
+    axios.post('http://localhost:3001/api/v1/pagamento', data)
+        .then(res => {
+            document.location = 'index.html';
+        })
+        .catch(error => alert('Error de cadastro:', 'ocorreu um error ao cadastrar', 'error'))
 }
-};
